b14b8d0882eef490d7192d0a1d12984973a9ca68
Avinash <avinash.kawade@pwc.com>
2017-01-24 18:14:27 -0800

    Initial Commit

---
+++ TelstraAssignment/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TelstraAssignment
+//
+//  Created by Avinash on 1/24/17.
+//  Copyright © 2017 Telstra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TelstraAssignment/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TelstraAssignment
+//
+//  Created by Avinash on 1/24/17.
+//  Copyright © 2017 Telstra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
